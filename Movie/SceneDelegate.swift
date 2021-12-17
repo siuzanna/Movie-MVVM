5448306624fe2b7520cdf18f9bb58bdb8e46d8de
siuzanna <64474454+siuzanna@users.noreply.github.com>
2021-12-17 18:01:23 +0600

    Delete Main Storyboard

--- Movie/SceneDelegate.swift
+++ Movie/SceneDelegate.swift
@@ -8,23 +8,22 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        if let windowScene = scene as? UIWindowScene {
-            let window = UIWindow(windowScene: windowScene)
-            let vc = UIViewController()
-                .embeddedInNavigationController()
-                .makeTransparent()
-                .removeTitle()
-//                .changeTintColor(to: Colors.clientMain.color)
-            window.backgroundColor = UIColor.white
-            window.rootViewController = vc
-            window.makeKeyAndVisible()
-            self.window = window
-        }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        let vc = UIViewController()
+            .embeddedInNavigationController()
+            .makeTransparent()
+            .removeTitle()
+            .changeTintColor(to: .red)
+        window.backgroundColor = UIColor.white
+        window.rootViewController = vc
+        window.makeKeyAndVisible()
+        self.window = window
     }
 }
 
