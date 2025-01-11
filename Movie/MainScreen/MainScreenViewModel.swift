1629c97eca8aaefdd2d46d437968bdca020684b9
Siuzanna Karagulova <siuzannakaragulova@gmail.com>
2025-01-11 15:54:22 +0600

    Feature: Add a local JSON file in case the API is removed

--- Movie/MainScreen/MainScreenViewModel.swift
+++ Movie/MainScreen/MainScreenViewModel.swift
@@ -47,21 +47,40 @@ extension MainScreenViewModel {
             urlRequest: MainScreenRouter.getAllPosts.createURLRequest(),
             successModel: [MovieDTO].self
         ) { [weak self] (result) in
-            guard let self else {return}
+            guard let self else { return }
             switch result {
             case .success(let model):
                 self.movieList = model
                 self.delegate?.updateCollectionView()
             case .badRequest(let error):
-                self.delegate?.showError(text: error.errors?.first ?? "Произошла ошибка")
+                self.delegate?.showError(text: error.errors?.first ?? "Error occurred")
                 debugPrint(#function, error)
+                self.fetchLocalJsonMovies()
             case .failure(let error):
                 self.delegate?.showError(text: error)
                 debugPrint(#function, error)
+                self.fetchLocalJsonMovies()
             }
         }
     }
-    
+
+    private func fetchLocalJsonMovies() {
+        guard let path = Bundle.main.path(forResource: "movies", ofType: "json") else {
+            debugPrint("Local JSON file 'move.json' not found.")
+            return
+        }
+
+        do {
+            let data = try Data(contentsOf: URL(fileURLWithPath: path))
+            let localMovies = try JSONDecoder().decode([MovieDTO].self, from: data)
+            self.movieList = localMovies
+            self.delegate?.updateCollectionView()
+        } catch {
+            debugPrint("Error decoding local JSON file: \(error)")
+            self.delegate?.showError(text: "Failed to load data from the local file")
+        }
+    }
+
     func getMoviesBySection(_ indexPath: Int) -> [MovieDTO] {
         return movieList.filter { $0.type == indexPath }
     }
